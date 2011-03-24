4ed856a3834ecf6d4cdaa843ab17db72173876d1
Olivier Louvignes <olivier@mg-crea.com>
2011-03-24 12:24:42 +0100

    Added time() function.

--- jquery.mg.screensaver.js
+++ jquery.mg.screensaver.js
@@ -81,4 +81,15 @@ $.fn.extend({
 
 });
 
+function time () {
+    // http://kevin.vanzonneveld.net
+    // +   original by: GeekFG (http://geekfg.blogspot.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: metjay
+    // +   improved by: HKM
+    // *     example 1: timeStamp = time();
+    // *     results 1: timeStamp > 1000000000 && timeStamp < 2000000000
+    return Math.floor(new Date().getTime() / 1000);
+}
+
 })(jQuery, console);
